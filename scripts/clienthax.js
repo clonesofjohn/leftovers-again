04abf48a048bef994442ca7981084537e11cf5f5
Martin Silbiger <martins@accretivetg.com>
2015-12-23 17:04:42 -0800

    Untested work on clienthax script

--- scripts/clienthax.js
+++ scripts/clienthax.js
@@ -1,4 +1,3 @@
-
 // ==UserScript==
 // @name         Pokemon Showdown Helper
 // @namespace    http://metal-heart.org/
@@ -15,6 +14,15 @@
 // ==/UserScript==
 console.log('hello from clienthax.js');
 
+// apply extra CSS
+// .switchmenu button {
+//   width: 150px;
+// }
+
+// .movemenu button {
+//   width: 306px;
+// }
+
 var ws = new window.WebSocket('ws://localhost:7331');
 var isOpen = false;
 var msgQueue = [];
@@ -26,6 +34,9 @@ ws.onopen = function() {
   ws.onmessage = function(msg) {
     console.log('received msg ', msg);
     $('.battle-log .inner').append('<p>' + msg.data + '</p>');
+    if (msg.data.moves) _onMoveData(msg.data.moves);
+    if (msg.data.opponent) _onOpponentData(msg.data.opponent);
+    if (msg.data.switches) _onSwitchData(msg.data.switches);
   };
 
   isOpen = true;
@@ -40,6 +51,11 @@ ws.onopen = function() {
 var callhome = function() {
   ws.send('>' + window.room.id + '\n|ask4help');
 };
+
+var clear = function() {
+  console.log('clearing out all lgn\'s');
+  $('.lgn').remove();
+}
 
 var helpExists = false;
 
@@ -63,10 +79,53 @@ var listen = function() {
       var helpButton = '<button class="help">HELP!!</button>';
       $('.battle-options div').prepend(helpButton);
       $('button.help').click(callhome);
+
+      // temporarily add a 'clear' button
+      var clearButton = '<button class="lgnclear">CLEAR</button>';
+      $('.battle-options div').prepend(clearButton);
+      $('button.lgnclear').click(clear);
+
       helpExists = true;
     }
 
   };
+};
+
+var _onMoveData = function(moves) {
+  for (var i = 0; i < moves.length; i++) {
+    $('.movemenu button[value=' + (i + 1) + '] small')
+      .first()
+      .after('<small class="lgn damage">' +
+        moves[i].damage +
+        '</small>');
+
+  }
+};
+
+var _opponentData = function(opponent) {
+  if(!opponent) return;
+  var arrows = '';
+  if(opponent.hasStrength) arrows += _getIcon('up-arrow');
+  if(opponent.hasWeakness) arrows += _getIcon('down-arrow');
+
+  $('.statbar.lstatbar strong')
+    .after('<small class="lgn">' + arrows + '</small>');
+};
+
+var _onSwitchData = function(switches) {
+  // value seems to be 0-5
+  // but is '[Species or Name],active' for the active mon
+  for (var i = 0; i < switches.length; i++) {
+    var searchVal = (switches[i].active)
+      ? '*active'
+      : i;
+    $('.switchmenu button[value="' + searchVal + '"] span.hpbar')
+      .before('<small class="lgn">something</small>');
+  }
+};
+
+var _getIcon = function(str) {
+  return '<i class="fa fa-' + str + '" />';
 }
 
 listen();
